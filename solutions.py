44df19bd601cac94a169f44ebc43cced2d2a091c
MATANBENSHABAT <matanbs2000@gmail.com>
2023-01-24 13:11:12 +0200

    second

--- solutions.py
+++ solutions.py
@@ -1,3 +1,4 @@
+# Price Check
 def priceCheck(products, productPrices, productSold, soldPrice):
     errors = 0
     products_dict = dict(zip(products, productPrices))
@@ -6,8 +7,10 @@ def priceCheck(products, productPrices, productSold, soldPrice):
             errors += 1
     return errors
 
+# SQL
+# I did not do the sql question because I know nosql, I would be happy to receive a nosql question if relevant
 
-
+# Recursive Digit “Summer”
 def recursive_digit_sum(n):
     if n == 0:
         return 0
